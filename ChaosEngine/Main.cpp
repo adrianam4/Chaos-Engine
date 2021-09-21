ab12ecab90146d330df4f76eb30b62258bd964b1
adrianam4 <adrian.aroca.joliver@gmail.com>
2021-09-21 16:11:41 +0200

    Revert "dll and lib updated"
    This reverts commit edd78c24e2ac03e2ff44e51926f4ade414acded0.

--- ChaosEngine/Main.cpp
+++ ChaosEngine/Main.cpp
@@ -55,8 +55,8 @@ int main(int argc, char ** argv)
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
 	// Setup Dear ImGui style
 	ImGui::StyleColorsDark();
