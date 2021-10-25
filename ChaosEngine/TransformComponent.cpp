6dca55e109ee773012ffaa8c53fdf1500e0a7009
adrianam4 <adrian.aroca.joliver@gmail.com>
2021-10-25 18:04:14 +0200

    rotations fix

--- ChaosEngine/TransformComponent.cpp
+++ ChaosEngine/TransformComponent.cpp
@@ -35,6 +35,9 @@ void ComponentTransform::Update()
 	transMatrix = aux.FromTRS(position, rotationQuat, scale);
 	transMatrix = transMatrix.Transposed();
 	App->editor->objectSelected->matrix = transMatrix.ptr();
+
+	App->editor->AddLog("Quat -> w:%f x:%f y:%f z:%f \n",rotationQuat.w, rotationQuat.x, rotationQuat.y, rotationQuat.z);
+	App->editor->AddLog("Angles -> x:%f y:%f z:%f \n",rotationEuler.x, rotationEuler.y, rotationEuler.z);
 }
 
 void ComponentTransform::Disable()
@@ -43,8 +46,6 @@ void ComponentTransform::Disable()
 
 void ComponentTransform::OnEditor(int i)
 {
-	rotationEuler = FromQuatToEuler(rotationQuat);
-
 	ImGui::TextColored(ImVec4(0, 0, 255, 255), "Position");
 	if (ImGui::DragFloat("Position X", &position.x))
 		Update();
@@ -62,11 +63,11 @@ void ComponentTransform::OnEditor(int i)
 		Update();
 
 	ImGui::TextColored(ImVec4(0, 0, 255, 255), "Rotation");
-	if (ImGui::DragFloat("Rotation X", &rotationEuler.x, 0.5f, -360.0f, 360.0f))
+	if (ImGui::DragFloat("Rotation X", &rotationEuler.x))
 		Update();
-	if (ImGui::DragFloat("Rotation Y", &rotationEuler.y, 0.5f, -360.0f, 360.0f))
+	if (ImGui::DragFloat("Rotation Y", &rotationEuler.y))
 		Update();
-	if (ImGui::DragFloat("Rotation Z", &rotationEuler.z, 0.5f, -360.0f, 360.0f))
+	if (ImGui::DragFloat("Rotation Z", &rotationEuler.z))
 		Update();
 }
 
@@ -76,18 +77,7 @@ Quat ComponentTransform::FromEulerToQuat(float3 eulerAngles)
 	eulerAngles.y = math::DegToRad(eulerAngles.y);
 	eulerAngles.z = math::DegToRad(eulerAngles.z);
 
-	float cy = cos(eulerAngles.z * 0.5);
-	float sy = sin(eulerAngles.z * 0.5);
-	float cp = cos(eulerAngles.y * 0.5);
-	float sp = sin(eulerAngles.y * 0.5);
-	float cr = cos(eulerAngles.x * 0.5);
-	float sr = sin(eulerAngles.x * 0.5);
-
-	Quat q;
-	q.w = cr * cp * cy + sr * sp * sy;
-	q.x = sr * cp * cy - cr * sp * sy;
-	q.y = cr * sp * cy + sr * cp * sy;
-	q.z = cr * cp * sy - sr * sp * cy;
+	Quat q = q.FromEulerXYZ(eulerAngles.x, eulerAngles.y, eulerAngles.z);
 
 	return q;
 }
@@ -96,22 +86,7 @@ float3 ComponentTransform::FromQuatToEuler(Quat quatAngles)
 {
 	float3 angles;
 
-	//(x-axis rotation)
-	double sinr_cosp = 2 * (quatAngles.w * quatAngles.x + quatAngles.y * quatAngles.z);
-	double cosr_cosp = 1 - 2 * (quatAngles.x * quatAngles.x + quatAngles.y * quatAngles.y);
-	angles.x = std::atan2(sinr_cosp, cosr_cosp);
-
-	//(y-axis rotation)
-	double sinp = 2 * (quatAngles.w * quatAngles.y - quatAngles.z * quatAngles.x);
-	if (std::abs(sinp) >= 1)
-		angles.y = std::copysign(M_PI / 2, sinp);
-	else
-		angles.y = std::asin(sinp);
-
-	//(z-axis rotation)
-	double siny_cosp = 2 * (quatAngles.w * quatAngles.z + quatAngles.x * quatAngles.y);
-	double cosy_cosp = 1 - 2 * (quatAngles.y * quatAngles.y + quatAngles.z * quatAngles.z);
-	angles.z = std::atan2(siny_cosp, cosy_cosp);
+	angles = quatAngles.ToEulerXYZ();
 
 	angles.x = math::RadToDeg(angles.x);
 	angles.y = math::RadToDeg(angles.y);
